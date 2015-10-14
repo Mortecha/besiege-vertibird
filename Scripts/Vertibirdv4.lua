cb2ea8eb4e8cec53bce8a514e81a2befbd40473d
Mortecha <sam.keightley@gmail.com>
2015-10-14 21:18:09 +1000

    Complete script rewrite now with OOP and FSM goodness

--- Scripts/Vertibirdv4.lua
+++ Scripts/Vertibirdv4.lua
@@ -1,245 +1,368 @@
+--------------------------------------------------------------------------------
+ -- TorqueGenerator class
+--------------------------------------------------------------------------------
+local TorqueGenerator = {}
+TorqueGenerator.__index = TorqueGenerator
+
+setmetatable(TorqueGenerator, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,
+})
+
+function TorqueGenerator.new(c)
+	local self = setmetatable({}, TorqueGenerator)
+	self.c = c -- torque generator components
+	self.longVal = 0
+	self.latVal = 0
+
+	function self.rotate(axis, speed)
+		for i, v in pairs(axis) do
+			besiege.setSliderValue(v, speed)
+		end
+	end
+
+	return self
+end
+
+--need to refactor this next
+function TorqueGenerator:update()
+	self.longVal = besiege.getRoll(self.c[4][3], self.c[4][4])
+	self.latVal = besiege.getPitch(self.c[4][1], self.c[4][2])
+
+	if self.longVal > 5 then
+		self.rotate(self.c[1], 2)
+	elseif self.longVal < -5 then
+		self.rotate(self.c[1], -2)
+	else
+		self.rotate(self.c[1], 0)
+	end
+
+	if self.latVal > 5 then
+		self.rotate(self.c[2], 2)
+	elseif self.latVal < -5 then
+		self.rotate(self.c[2], -2)
+	else
+		self.rotate(self.c[2], 0)
+	end
+end
 
 --------------------------------------------------------------------------------
--- COMPONENT DEFINES
+ -- Engine class
 --------------------------------------------------------------------------------
--- Due to the fact that if you make any change to the vehicle, the order and ID's
--- of the components can change. This should shield the script from this as only 
--- the ID's will need to be changed here and not throughout the entire script. 
+local Engine = {}
+Engine.__index = Engine
 
-------------------------------
--- Gyroscope Component IDs
-------------------------------
-local rollGyro = {107, 108, 109, 110, 111, 116, 115, 114, 113, 112}
-local yawGyro = {104, 103, 102, 101, 100, 99, 165, 164}
-local pitchGyro = {122, 123, 124, 125, 126, 121, 120, 119, 118, 117}
+setmetatable(Engine, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,
+})
 
--- points
-local fGyroPt = 106
-local cTGyroPt = 127 
-local cBGyroPt = 0
-local bGyroPt = 105
-local lGyroPt = 97
-local rGyroPt = 27
+function Engine.new(c)
+	local self = setmetatable({}, Engine)
+	self.c = c -- engine components
+	return self
+end
 
-------------------------------
+function Engine:adjThrustTo(val)
+	for i, v in pairs(self.c) do
+		v[2] = v[3] / 100 * val
+		besiege.setSliderValue(v[1], v[2])
+	end
+end
+
+--------------------------------------------------------------------------------
+ -- Flight control state machine
+--------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------
+-- Torque Gen Component IDs
+--------------------------------------------------------------------------------
+local tGenComponents = 
+{
+	{107, 108, 109, 110, 111, 116, 115, 114, 113, 112}, --long
+	{122, 123, 124, 125, 126, 121, 120, 119, 118, 117}, --lat
+	{104, 103, 102, 101, 100, 99, 165, 164}, --perp
+	{106, 105, 97, 27, 127, 0} -- points - f, b, l, r, t, btm
+}
+
+--------------------------------------------------------------------------------
 -- Left Engine Component IDs
-------------------------------
-local lTEngSBlock = 52
-local lTEngSBlade = 54
-local lTEngWheel = 50
-local lBEngWheel = 56
-local lBEngSBlade = 61
-local lBEngSBlock = 59
+--------------------------------------------------------------------------------
+local lTSBlade = 54
+local lBSBlade = 61
+local lEngineComponents = 
+{
+	-- componentID, sliderVal, maxSliderVal
+	{50, 0, 2},		
+	{lTSBlade, 0, 0.85}, 	
+	{52, 0, 2}, 		
+	{56, 0, 2}, 		
+	{lBSBlade, 0, 0.85}, 	
+	{59, 0, 2} 		
+}
 
-------------------------------
+--------------------------------------------------------------------------------
 -- Right Engine Component IDs
-------------------------------
-local rTEngSBlock = 51
-local rTEngSBlade = 53
-local rTEngWheel = 49
-local rBEngWheel = 57
-local rBEngSBlade = 60
-local rBEngSBlock = 58
+--------------------------------------------------------------------------------
+local rTSBlade = 53
+local rBSBlade = 60
+local rEngineComponents = 
+{
+	-- componentID, sliderVal, maxSliderVal
+	{49, 0, 2}, 		
+	{rTSBlade, 0, 0.85},	
+	{51, 0, 2}, 		
+	{57, 0, 2}, 		
+	{rBSBlade, 0, 0.85}, 	
+	{58, 0, 2} 		
+}
+
+--------------------------------------------------------------------------------
+-- required because you cant set sawblade slider values to 0 directly in the .bsg file:(
+besiege.setSliderValue(lTSBlade, 0) -- left top sawblade
+besiege.setSliderValue(lBSBlade, 0) -- left bottom sawblade
+besiege.setSliderValue(rTSBlade, 0) -- right top sawblade
+besiege.setSliderValue(rBSBlade, 0) -- right bottom sawblade
 
 --------------------------------------------------------------------------------
 
-besiege.setSliderValue(lTEngSBlade, 0) -- left top sawblade
-besiege.setSliderValue(lBEngSBlade, 0) -- left bottom sawblade
-besiege.setSliderValue(rTEngSBlade, 0) -- right top sawblade
-besiege.setSliderValue(rBEngSBlade, 0) -- right bottom sawblade
+local tG = TorqueGenerator(tGenComponents)
+local lEng = Engine(lEngineComponents)
+local rEng = Engine(rEngineComponents)
 
-local engInitLog = false
-local engStart = false
-local engStartComplete = false
+local lEngThrustPercent = 0
+local rEngThrustPercent = 0
+local targetAlt = 0
 
--- engine component speed initialisations
-local lEngWheelSpeed = {0}
-local lEngSBladeSpeed = {0}
-local lEngSBlockSpeed = {0}
-local rEngWheelSpeed = {0}
-local rEngSBladeSpeed = {0}
-local rEngSBlockSpeed = {0}
+local enginesActivateLog = false
+local enginesDeactivateLog = false
+local enginesIdleLog = false
 
--- bounds for engine component speeds to maintain position on ground
-local engWheelIdleSpeed = 0.25
-local engSBladeIdleSpeed = 0.3
-local engSBlockIdleSpeed = 0.25
+function enginesInactive()
+	besiege.log("Engines inactive")
+end
 
--- engine component idle states
-local lEngWheelIdling = {false}
-local lEngSBladeIdling = {false}
-local lEngSBlockIdling = {false}
-local rEngWheelIdling = {false}
-local rEngSBladeIdling = {false}
-local rEngSBlockIdling = {false}
+function activateEngines()
+	local idleThrustPercentage = 20
+	enginesDeactivateLog = false
 
--- bounds for engine component speeds to prevent blade damage
-local engWheelMaxSpeed = 2
-local engSBladeMaxSpeed = 0.85
-local engSBlockMaxSpeed = 2
+	if enginesActivateLog == false then
+		enginesActivateLog = true
+		besiege.log("Engines activated")	
+	end
 
--- bound for engine accent speed to reduce bopping when machine tries to accend to specific alt
-local engWheelAccSpeed = 0.3
-local engSBladeAccSpeed = 0.4
-local engSBlockAccSpeed = 0.3
+	if lEngThrustPercent < idleThrustPercentage then
+		lEng:adjThrustTo(rEngThrustPercent + 0.2)
+		lEngThrustPercent = lEngThrustPercent + 0.2
+		if(lEngThrustPercent > idleThrustPercentage) then
+			lEngThrustPercent = idleThrustPercentage
+		end
+	end
 
--- bounds for engine component speeds to prevent vehicle falling out of the sky during descents
-local engWheelDescentSpeed = 0.2
-local engSBladeDescentSpeed = 0.25
-local engSBlockDescentSpeed = 0.2
+	if rEngThrustPercent < idleThrustPercentage then
+		rEng:adjThrustTo(rEngThrustPercent + 0.2)
+		rEngThrustPercent = rEngThrustPercent + 0.2
+		if(rEngThrustPercent > idleThrustPercentage) then
+			rEngThrustPercent = idleThrustPercentage
+		end
+	end
 
--- specifications for engine component hover speeds 
+	-- if idling speed reached by both engines
+	if(lEngThrustPercent == idleThrustPercentage and rEngThrustPercent == idleThrustPercentage) then
+		changeState("activatingEngines", "engineActivationComplete")
+		enginesActivateLog = false
+	end
+end
 
--- variables concerning altitude
-local plannedAlt = 1
-local maxAlt = 100
+function idling()
+	if enginesIdleLog == false then
+		enginesIdleLog = true
+		besiege.log("Engines Idling")	
+	end
+end
+
+function deactivateEngines()
+	enginesActivateLog = false
+	enginesIdleLog = false
+	if enginesDeactivateLog == false then
+		enginesDeactivateLog = true
+		besiege.log("Engines deactivating")	
+	end
+	
+	if lEngThrustPercent > 0 then
+		lEng:adjThrustTo(rEngThrustPercent - 0.2)
+		lEngThrustPercent = lEngThrustPercent - 0.2
+		if(lEngThrustPercent < 0) then
+			lEngThrustPercent = 0
+		end
+	end
+	
+	if rEngThrustPercent > 0 then
+		rEng:adjThrustTo(rEngThrustPercent - 0.2)
+		rEngThrustPercent = rEngThrustPercent - 0.2
+		if(rEngThrustPercent < 0) then
+			rEngThrustPercent = 0
+		end
+	end
+	
+	if(lEngThrustPercent == 0 and rEngThrustPercent == 0) then
+		changeState("inactive", "enginesDeactivated")
+		enginesDeactivateLog = false
+	end
+end
+
+function ascFromIdle()
+	-- allows for retraction of landing gear once script lib allows for piston control 
+	-- increase altitude directly from here
+
+	if enginesIdleLog == false then
+		enginesIdleLog = true
+		besiege.log("Activating engines")	
+	end
+	--transitionAltTo(targetAlt)
+end
+
+function hover()
+	besiege.log("Hovering")
+end
+
+function dscFromAsc()
+	besiege.log("Descending from ascending")
+end
+
+function ascFromHover()
+	besiege.log("Ascending after hover")
+end
+
+function dscFromHover()
+	besiege.log("Descending after hover")
+end
+
+function ascFromDsc()
+	besiege.log("Ascending from Descending")
+end
+
+function hoverFromDsc()
+	besiege.log("Hovering after descent")
+end
+
+function land()
+	besiege.log("Landing")
+end
+
+function idleFromLanding()
+	besiege.log("Idling after landing")
+end
+
+function transitionAltTo(val)
+	if besiege.getPositionY(0) < targetAlt then
+		if lEngThrustPercent < 50 then
+			lEng:adjThrustTo(lEngThrustPercent + 0.2)
+			lEngThrustPercent = lEngThrustPercent + 0.2
+		end
+		if rEngThrustPercent < 50 then
+			rEng:adjThrustTo(rEngThrustPercent + 0.2)
+			rEngThrustPercent = rEngThrustPercent + 0.2
+		end
+	else -- besiege.getPositionY(0) > targetAlt implied
+		if lEngThrustPercent > 20 then
+			lEng:adjThrustTo(lEngThrustPercent - 0.2)
+			lEngThrustPercent = lEngThrustPercent - 0.2
+		end
+		if rEngThrustPercent > 20 then
+			rEng:adjThrustTo(rEngThrustPercent - 0.2)
+			rEngThrustPercent = rEngThrustPercent - 0.2
+		end
+	end
+end
+
+function FSM(t)
+	local a = {}
+	for _,v in ipairs(t) do
+		local old, event, new, action = v[1], v[2], v[3], v[4]
+		if a[old] == nil then 
+			a[old] = {} 
+		end
+		a[old][event] = {new = new, action = action}
+	end
+	return a
+end
+
+
+fsm = FSM
+{
+	{"inactive", "enginesDeactivated", "inactive", enginesInactive}, -- start state
+	{"inactive", "enginesActivationBegin", "activatingEngines", activateEngines},
+	{"activatingEngines", "enginesDeactivatedDuringActivation", "deactivatingEngines", deactivateEngines},
+	{"deactivatingEngines", "enginesActivatedDuringDeactivation", "activatingEngines", activateEngines},
+	{"activatingEngines", "engineActivationComplete", "idle", idling},
+	{"idle", "engineDeactivatedDuringIdle", "deactivatingEngines", deactivateEngines},
+
+
+	--{"deactivatingEngines", "engineShutdownComplete", "inactive", enginesInactive},
+	--{"deactivatingEngines", "enginePoweringUpFromShutdown", "activatingEngines", activateEngines},
+	--{"idle", "stopEnginesFromIdle", "deactivatingEngines", deactivateEngines},
+
+
+--	{"idle", "altRaisedFromIdle", "ascend", ascFromIdle},
+--	{"ascend", "altReachedFromIdle", "hover", hover},
+--	{"ascend", "altLoweredFromAscent", "descend", dscFromAsc},
+--	{"hover", "altRaisedFromHover", "ascend", ascFromHover},
+--	{"hover", "altLoweredFromHover", "descend", dscFromHover},
+--	{"descend", "altRaisedFromDescent", "ascend", ascFromDsc},
+--	{"descend", "altReachedFromDescent", "hover", hover},
+--	{"descend", "altLoweredToZero", "landing", land},
+--	{"landing", "altReachedForLanding", "idle", idleFromLanding},
+}
+
+local a = fsm["inactive"]["enginesDeactivated"]
+a.action()
+state = a.new
 
 besiege.onKeyDown = function(keyCode)
 	if keyCode == besiege.keyCodes.z then
-		if engStart == false then
-			engStart = true
-		elseif engStart == true then
-			engStart = false
+		if state == "inactive" then
+			changeState("inactive","enginesActivationBegin")
+		elseif state == "activatingEngines" then
+			changeState("activatingEngines", "enginesDeactivatedDuringActivation")
+		elseif state == "deactivatingEngines" then
+			changeState("deactivatingEngines", "enginesActivatedDuringDeactivation")
+		elseif state == "idle" then
+			changeState("idle", "engineDeactivatedDuringIdle")
 		end
 	end
 end
 
 besiege.onKeyHeld = function(keyCode)
 	if keyCode == besiege.keyCodes.keypadPlus then
-		if plannedAlt < maxAlt then
-			plannedAlt = plannedAlt + 10
-			besiege.log("Ascending to: " .. plannedAlt .. " units")
-		end
+		targetAlt = targetAlt + 5
 	end 
 
 	if keyCode == besiege.keyCodes.keypadMinus then
-		if plannedAlt >= 1 then
-			plannedAlt = plannedAlt - 10
-			besiege.log("Descending to: " .. plannedAlt .. " units")
+		targetAlt = targetAlt - 5
+		if targetAlt < 0 then
+			targetAlt = 0
 		end
 	end
 end
 
 besiege.onUpdate = function()
-	if engStart == true then
-		if engStartComplete == false then
-			if engInitLog == false then
-				engInitLog = true
-				besiege.log("Starting engines - Beginning start sequence")
-			end
-			startEngines()
-		end
+	tG:update()
+
+	if state == "activatingEngines" then
+		activateEngines()
+	elseif state == "deactivatingEngines" then
+		deactivateEngines()
 	end
 
-	if besiege.getRoll(lGyroPt, rGyroPt) > 5 then
-		adjAxis(rollGyro, 2)
-	elseif besiege.getRoll(lGyroPt, rGyroPt) < -5 then
-		adjAxis(rollGyro, -2)
-	else
-		adjAxis(rollGyro, 0)
-	end
-
-	if besiege.getPitch(fGyroPt, bGyroPt) > 5 then
-		adjAxis(pitchGyro, 2)
-
-	elseif besiege.getPitch(fGyroPt, bGyroPt) < -5 then
-		adjAxis(pitchGyro, -2)
-	else
-		adjAxis(pitchGyro, 0)
-	end
-
-	if besiege.getPositionY(0) < plannedAlt then
-		increaseThrust()
-	elseif besiege.getPositionY(0) > plannedAlt then
-		decreaseThrust()
-	end
 end
 
-function startEngines()
-	increaseComponentSpeed(lTEngWheel, lBEngWheel, lEngWheelSpeed, engWheelIdleSpeed, .004, lEngWheelIdling)
-	increaseComponentSpeed(lTEngSBlade, lBEngSBlade, lEngSBladeSpeed, engSBladeIdleSpeed, .004, lEngSBladeIdling)
-	increaseComponentSpeed(lTEngSBlock, lBEngSBlock, lEngSBlockSpeed, engSBlockIdleSpeed, .004, lEngSBlockIdling)
 
-	increaseComponentSpeed(rTEngWheel, rBEngWheel, rEngWheelSpeed, engWheelIdleSpeed, .004, rEngWheelIdling)
-	increaseComponentSpeed(rTEngSBlade, rBEngSBlade, rEngSBladeSpeed, engSBladeIdleSpeed, .004, rEngSBladeIdling)
-	increaseComponentSpeed(rTEngSBlock, rBEngSBlock, rEngSBlockSpeed, engSBlockIdleSpeed, .004, rEngSBlockIdling)
-
-	if lEngWheelIdling[1] and lEngSBladeIdling[1] and lEngSBlockIdling[1] then
-		if rEngWheelIdling[1] and rEngSBladeIdling[1] and rEngSBlockIdling[1] then
-			engStartComplete = true
-			besiege.log("Engines idling - Start sequence complete")
-		end 
-	end 
-end
-
-function adjAxis(gyro, speed)
-	for i, v in pairs(gyro) do
-		besiege.setSliderValue(v, speed)
-	end
-end
-
-function increaseThrust()
-	if engStartComplete == true then
-		increaseComponentSpeed(lTEngWheel, lBEngWheel, lEngWheelSpeed, engWheelAccSpeed, .008)
-		increaseComponentSpeed(lTEngSBlade, lBEngSBlade, lEngSBladeSpeed, engSBladeAccSpeed, .008)
-		increaseComponentSpeed(lTEngSBlock, lBEngSBlock, lEngSBlockSpeed, engSBlockAccSpeed, .008)
-
-		increaseComponentSpeed(rTEngWheel, rBEngWheel, rEngWheelSpeed, engWheelAccSpeed, .008)
-		increaseComponentSpeed(rTEngSBlade, rBEngSBlade, rEngSBladeSpeed, engSBladeAccSpeed, .008)
-		increaseComponentSpeed(rTEngSBlock, rBEngSBlock, rEngSBlockSpeed, engSBlockAccSpeed, .008)
-	end
-end 
-
-function decreaseThrust()
-	if engStartComplete == true then
-		decreaseComponentSpeed(lTEngWheel, lBEngWheel, lEngWheelSpeed, engWheelDescentSpeed, .008)
-		decreaseComponentSpeed(lTEngSBlade, lBEngSBlade, lEngSBladeSpeed, engSBladeDescentSpeed, .008)
-		decreaseComponentSpeed(lTEngSBlock, lBEngSBlock, lEngSBlockSpeed, engSBlockDescentSpeed, .008)
-
-		decreaseComponentSpeed(rTEngWheel, rBEngWheel, rEngWheelSpeed, engWheelDescentSpeed, .008)
-		decreaseComponentSpeed(rTEngSBlade, rBEngSBlade, rEngSBladeSpeed, engSBladeDescentSpeed, .008)
-		decreaseComponentSpeed(rTEngSBlock, rBEngSBlock, rEngSBlockSpeed, engSBlockDescentSpeed, .008)
-	end 
-end 
-
--- componentID - the component to increase speed of
--- inverseComponentID - OPTIONAL - for counterprop aircraft where component has matching inverse component
-	-- must specify nil when not in use
--- componentSpeed - components current speed
--- maxSpeed - the limit to how fast the component can spin for state
--- incrementAmount - the amount to increment speed by
--- state - OPTIONAL - the state you wish to make true when if condition false
-function increaseComponentSpeed(componentID, inverseComponentID, componentSpeed, maxSpeed, incrementAmount, state)
-	if componentSpeed[1] < maxSpeed then
-		componentSpeed[1] = componentSpeed[1] + incrementAmount
-		besiege.setSliderValue(componentID, componentSpeed[1])
-		if inverseComponentID ~= nil then
-			besiege.setSliderValue(inverseComponentID, componentSpeed[1])
-		end
-	else
-		if state ~= nil then
-			state[1] = true
-		end
-	end
-end
-
--- componentID - the component to decrease speed of
--- inverseComponentID - OPTIONAL - for counterprop aircraft where component has matching inverse component
-	-- must specify nil when not in use
--- componentSpeed - components current speed
--- maxSpeed - the limit to how slow the component can spin for state
--- incrementAmount - the amount to decriment speed by
--- state - OPTIONAL - the state you wish to make true when if condition false 
-function decreaseComponentSpeed(componentID, inverseComponentID, componentSpeed, minSpeed, decrimentAmount, state )
-	if componentSpeed[1] > minSpeed then
-		componentSpeed[1] = componentSpeed[1] - decrimentAmount
-		besiege.setSliderValue(componentID, componentSpeed[1])
-		if inverseComponentID ~= nil then
-			besiege.setSliderValue(inverseComponentID, componentSpeed[1])
-		end
-	else 
-		if state ~= nil then
-			state[1] = true
-		end
-	end
+function changeState(oldState, event)
+	a = fsm[oldState][event]
+	a.action()
+	state = a.new
 end
